e48921977cbb5654084246154576f42e788e7bf5
Prerna Sawant <155561176+prernasawant2127@users.noreply.github.com>
2025-12-19 13:32:17 +0530

    Bubble Sort
    #DataStructures #BubbleSort #CProgramming #SortingAlgorithm #DSA #BeginnerProject #Array #ConsoleApplication

---
+++ Bubble Sort.c
@@ -0,0 +1,21 @@
+//
+#include <stdio.h>
+
+int main() {
+    int a[] = {5, 1, 4, 2, 8}, n = 5;
+
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (a[j] > a[j+1]) {
+                int temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
